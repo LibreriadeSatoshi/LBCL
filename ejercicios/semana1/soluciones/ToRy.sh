7888c3039f94a06bc5e63fc38050346f389aee14
ToRy <129023353+ToRyVand@users.noreply.github.com>
2023-11-09 22:56:15 -0500

    Update ToRy.sh

--- ejercicios/semana1/soluciones/ToRy.sh
+++ ejercicios/semana1/soluciones/ToRy.sh
@@ -2,26 +2,27 @@
 #Shell Instalacion de Bitcoin Core 25.0 -Regtest + Minado.
 #By: ToRy
 
+#Colores
+endColour="\033[0m\e[0m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+redColour="\e[0;31m\033[1m"
 
 
 echo            ACTUALIZAR MAQUINA:
-
-echo            sudo apt update
-echo            sudo apt upgrade -y
-
-
+      
 
 echo            INSTALACION DE PAQUETES:
 	
-echo 		apt-get install -y bc jq autoconf file gcc libc-dev make g++ pkgconf re2c git libtool automake gcc xxd
+ 		apt-get install -y bc jq autoconf file gcc libc-dev make g++ pkgconf re2c git libtool automake gcc xxd
 	
 	
-echo            wget https://bitcoincore.org/bin/bitcoin-core-25.0/bitcoin-25.0-x86_64-linux-gnu.tar.gz
+                wget https://bitcoincore.org/bin/bitcoin-core-25.0/bitcoin-25.0-x86_64-linux-gnu.tar.gz
 		
 
 echo          FIRMAS Y VERIFICACION
 	
-echo		wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS
+		wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS
 		wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS.asc
 		sha256sum --ignore-missing --check SHA256SUMS
 		echo -e "$YELL Verificación exitosa del checksum de los archivos $NC"
@@ -34,7 +35,7 @@ echo		wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS
 
 echo           COPIA DE BINARIOS /usr/local/bin/."
  
-echo		tar -xvf bitcoin-25.0-x86_64-linux-gnu.tar.gz
+		tar -xvf bitcoin-25.0-x86_64-linux-gnu.tar.gz
 		sudo install -m 0755 -o root -g root -t /usr/local/bin bitcoin-25.0/bin/*
 		#rm -r $HOME/bitcoin*
 		#rm $HOME/SHA256SUMS*
@@ -43,9 +44,9 @@ echo		tar -xvf bitcoin-25.0-x86_64-linux-gnu.tar.gz
 
 echo           CREAR ARCHIVO bitcoin.conf  /home/<nombre-de-usuario>/.bitcoin/. 
  
-echo		mkdir $HOME/.bitcoin
+		mkdir $HOME/.bitcoin
 		touch $HOME/.bitcoin/bitcoin.conf
-echo            nano bitcoin.conf
+                nano bitcoin.conf
 
 		echo "regtest=1" 
 		echo "fallbackfee=0.0001" 
@@ -55,24 +56,24 @@ echo            nano bitcoin.conf
 echo         GUARDAR ARCHIVO 
 
 
-echo         INICIAR BITCOIND.... ;)   
+             INICIAR BITCOIND.... ;)   
 	        
 
-echo          :~/bitcoin-25.0$ bitcoind -regtest -daemon -fallbackfee=1.0 -maxtxfee=1.1
+              :~/bitcoin-25.0$ bitcoind -regtest -daemon -fallbackfee=1.0 -maxtxfee=1.1
               Bitcoin Core starting
 
 echo          COMPROBAR ACTIVIDAD RED:
-echo          :~/bitcoin-25.0$ bitcoin-cli -regtest getnetworkinfo
+              :~/bitcoin-25.0$ bitcoin-cli -regtest getnetworkinfo
 echo          #aqui veremos si estamos conectados correctamente
 
 echo          VERIFICAR SI HAY WALLETS CREADAS:
-echo          :~/bitcoin-25.0$ bitcoin-cli -regtest listwallets
+              :~/bitcoin-25.0$ bitcoin-cli -regtest listwallets
                [
                ]
 echo          #si no hemos manipulado antes, estara vacio!
 
 echo          CREANDO NUEVA WALLET "Miners":   
-echo          :~/bitcoin-25.0$ bitcoin-cli -regtest -named createwallet wallet_name="Miners"
+              :~/bitcoin-25.0$ bitcoin-cli -regtest -named createwallet wallet_name="Miners"
               {
               "name": "Miners",
               "warnings": [
@@ -80,14 +81,14 @@ echo         #aqui creamos nuestra primera wallet
     
 
 echo         CREANDO NUEVA WALLET "Traders":   
-echo          :~/bitcoin-25.0$ bitcoin-cli -regtest -named createwallet wallet_name="Traders"
+              :~/bitcoin-25.0$ bitcoin-cli -regtest -named createwallet wallet_name="Traders"
               {
               "name": "Traders",
               "warnings": [
 echo         #aqui creamos nuestra segunda wallet
     
 echo         CREANDO DIRECCION PARA RECEPCION: 
-echo         :~/bitcoin-25.0$ bitcoin-cli -regtest -rpcwallet=Miners getnewaddress "Recompensa de Minería"
+             :~/bitcoin-25.0$ bitcoin-cli -regtest -rpcwallet=Miners getnewaddress "Recompensa de Minería"
 echo         #nuestra primera direccion de recepcion!
 
 echo         COMPROBANDO WALLETS CREADAS:
@@ -99,84 +100,84 @@ echo         #efectivamente vemos nuestras wallets
 
 
 echo         MINANDO NUESTRO PRIMER BLOQUE:
-echo         :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" -generate 101
+             :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" -generate 101
 echo         #¡aqui veremos nuestros primeros bloques,ademas anexamo la wallet de recepcion "Miners"!!! ;)
 
 
 echo         VERIFICANDO RECEPCION COINBASE:
-echo         :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" getbalance  
+             :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" getbalance  
 echo         #veremos la recompensa minera depositada en la wallet!
 
 
 echo         CRENADO DIRECCION DE RECEPCION WALLET Traders
-echo         :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet=Traders getnewaddress "Recibido"
+             :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet=Traders getnewaddress "Recibido"
              #veremos nuestra nueva direccion!
 
 
 echo         ENVIO ENTRE WALLETS Miners to --- Traders:
-echo         :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet=Miners sendtoaddress  "agregar  direcion (Recibido)" "!agregar valor"
+             :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet=Miners sendtoaddress  "agregar  direcion (Recibido)" "!agregar valor"
 echo         #aqui se genera No. identifica la transaccion TXID
 
 
 echo         VERIFICANDO NUEVO SALDO EN LA WALLET Miners:
-echo         :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" getbalance
+             :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" getbalance
 echo         #de ir por buen camino, nuestro saldo en la wallet Miners se reduce...
 
 
 
 echo         VERIFICANDO NUEVO SALDO EN LA WALLET Traders:
-echo         :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Traders" getbalance
+             :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Traders" getbalance
 echo         #0.00000000  oh oh! sin saldo! normal ;) ahora la transaccion esta en la mempool... 
 
 echo         COMPROBANDO LA MEMPOOL:
-echo         :~/.bitcoin/regtest$ bitcoin-cli -regtest  getmempoolentry "#TXID de envio "
+             :~/.bitcoin/regtest$ bitcoin-cli -regtest  getmempoolentry "#TXID de envio "
 echo         #veremos toda la informacion relacionada a TXID y su estado por confirmar...
 
 
 echo         GENERENDO UN NUEVO BLOQUE:
-echo         :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" -generate 1
+             :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" -generate 1
 echo         #veremos un bloque mas minado, esto nos ayudara a confirmar la transaccion anterior.
 
 
-echo        :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" getbalance
+             :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" getbalance
 echo        #nuevo balance depositado en la wallet "Miners"
 
 
 echo        CONFIRMADO TXID CON NUEVO BALANCE:
-echo        :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Traders" getbalance
+            :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Traders" getbalance
 echo        #al minarse un nuevo bloque, la direcion nos muestra nuevo Balance...
             finamenlte recibimos el valor, y la blockchain funciona ¡Eureka!
 
 
 echo        INFORMACION DE TRANSACCIONES:
-echo        :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" gettransaction "TXID"
+            :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" gettransaction "TXID"
 echo        #obtendremos detalle, de la transaccion como: "amount" "fee" "confirmations"
             "blockhash" "blockheight"
 
 
 echo        GENERANDO 6 BLOQUE +
-echo        :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" -generate 6
+            :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" -generate 6
 echo        #de esta manera la primera transaccion ya sera irreversible!
 
 
 echo       REVISANDO BALANCES:
-echo       :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" getbalance
+           :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Miners" getbalance
 echo       #nuevo valor a + bloques minados mayor recompensa minera depositada.
 
 echo     
-echo       :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Traders" getbalancel 
+           :~/.bitcoin/regtest$ bitcoin-cli -regtest -rpcwallet="Traders" getbalancel 
 echo       #el valor continua OK!
 
 
 echo       VERIFICANDO WALLETS:
-echo       :~/.bitcoin/regtest$ bitcoin-cli -regtest listwallets
+           :~/.bitcoin/regtest$ bitcoin-cli -regtest listwallets
            [
            "Miners",
            "Traders",
 echo       #finalmente, logramos minar bloques y transferir valor!!!
 
 
-echo       :~/bitcoin-25.0$ bitcoin-cli -regtest stop
+           :~/bitcoin-25.0$ bitcoin-cli -regtest stop
 echo       Bitcoin Core stopping
 
  
