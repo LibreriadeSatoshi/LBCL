f2fccd17215ee74003d82d05b0cd5e6e74b9c22e
Rosasinespinas <rosa.fenollar@gmail.com>
2023-11-25 20:38:12 -0500

    ActualizarSemana1

--- ejercicios/semana1/soluciones/RosaFen_semana1.sh
+++ ejercicios/semana1/soluciones/RosaFen_semana1.sh
@@ -1,114 +1,146 @@
 #!/bin/bash
-#
-directorio=$(pwd)
-#
+
 # Bajar Bitcoin, hashes y firmas
-sudo wget https://bitcoincore.org/bin/bitcoin-core-25.0/bitcoin-25.0-x86_64-linux-gnu.tar.gz
-sudo wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS.asc
-sudo wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS
-#
+echo
+echo "Punto 1.1. Descargar los binarios"
+wget https://bitcoincore.org/bin/bitcoin-core-25.1/bitcoin-25.1-x86_64-linux-gnu.tar.gz
+wget https://bitcoincore.org/bin/bitcoin-core-25.1/SHA256SUMS.asc
+wget https://bitcoincore.org/bin/bitcoin-core-25.1/SHA256SUMS
+
 # Verificacion sumas y llaves
-sha256sum --ignore-missing --check SHA256SUMS
-sha256sum bitcoin-25.0-x86_64-linux-gnu.tar.gz
-echo "Verificación exitosa de la firma binaria"
+echo
+echo "Punto 1.2. Verificar los binarios y mostrar mensaje por la terminal"
+verificacion1=$(sha256sum --ignore-missing --check SHA256SUMS) #resultado dice bitcoin-25....OK
+verificacion21=$(sha256sum bitcoin-25.1-x86_64-linux-gnu.tar.gz) #resultado es el shasum y bitcoin....gnu.tar.gz
+verificacion22=$(cat SHA256SUMS | grep bitcoin-25.1-x86_64-linux-gnu.tar.gz) # 21 y 22 tienen que ser iguales
+if [[ $verificacion1 == *"OK"* ]] ; then
+    echo "----------------------------------------"
+    echo "Verificacion exitosa de la firma binaria"
+    echo "----------------------------------------"
+else
+    echo "Error en firma binaria"
+fi
+if [[ $verificacion21 == $verificacion22 ]] ; then
+    echo "------------------------------"
+    echo "Verificacion exitosa SHA256SUM"
+    echo "------------------------------"
+else
+    echo "Error en SHA256SUM"
+fi
 git clone https://github.com/bitcoin-core/guix.sigs
 gpg --import guix.sigs/builder-keys/*
-gpg --verify SHA256SUMS.asc # gpg --verify SHA256SUMS.asc bitcoin-25.0-x86_64-linux-gnu.tar.gz
-echo -n "¿Firma correcta? ¿Continuar?(s/n)"
-read -N 1 continuar
-if [ $continuar = "n" ] ; then
-    echo
-    exit 1
+# gpg --verify SHA256SUMS.asc | grep Good # gpg --verify SHA256SUMS.asc bitcoin-25.1-x86_64-linux-gnu.tar.gz
+# if sha256sum --ignore-missing --check SHA256SUMS; then
+verificacion3=$(gpg --verify SHA256SUMS.asc 2>&1 | grep "Good")
+if [[ $verificacion3 == *"Good"* ]] ; then # if [[ -n $verificacion3 ]]; then
+    echo "------------------------------------------"
+    echo "Verificacion exitosa de las firmas con gpg"
+    echo "------------------------------------------"
+else
+    echo "Error en firmas usando gpg"
 fi
-echo "continuamos"
-#
-#Mover binario
-mv bitcoin-25.0-x86_64-linux-gnu.tar.gz /usr/local/bin
-cd /usr/local/bin
-#
+
 #Instalar Bitcoin Core
-tar xzf bitcoin-25.0-x86_64-linux-gnu.tar.gz
-echo "Instalando Bitcoin Core 25.0 puede tomar unos minutos"
-sudo install -m 0755 -o root -g root -t /usr/local/bin bitcoin-25.0/bin/*
-#
+echo
+echo "Punto 1.3. Copiar los binarios en la carpeta /usr/local/bin"
+tar xzf bitcoin-25.1-x86_64-linux-gnu.tar.gz
+echo "Instalando Bitcoin Core 25.1 puede tomar unos minutos"
+install -m 0755 -o root -g root -t /usr/local/bin bitcoin-25.1/bin/*
+
 #Crear archivo bitcoin.conf y definir parametros para que se ejecute en regtest
-cd /home
-cd ~/.bitcoin
-cat >> bitcoin.conf << EOF
+echo
+echo "Punto 2.1. Crear archivo bitcoin.conf y agregar lo necesario para ejecutar en modo regtest"
+mkdir ~/.bitcoin
+cat >> ~/.bitcoin/bitcoin.conf << EOF
 regtest=1
 fallbackfee=0.0001
 server=1
 txindex=1
 EOF
-#
+
 #Arrancar bitcoind
-/usr/local/bin/bitcoind -daemon
+echo
+echo "Punto 2.2. Iniciar bitcoind"
+bitcoind -daemon
 echo "Bitcoind se está ejecutando en modo demonio"
-echo -n "Puse una tecla para continuar"
-read
-#
+sleep 1
+
 #Crear billeteras
-bitcoin-cli -named createwallet wallet_name="Trader" descriptors=true #Revisar aqui que warinng está vacio
-bitcoin-cli unloadwallet "Trader"
-bitcoin-cli -named createwallet wallet_name="Miner" descriptors=true #Revisar aqui que warning está vacio
-#
+echo
+echo "Punto 2.3. Crear billeteras Miner y Trader"
+bitcoin-cli -named createwallet wallet_name="Miner" descriptors=true
+bitcoin-cli -named createwallet wallet_name="Trader" descriptors=true
+
 #Generar direccion
-Mineria=$(bitcoin-cli getnewaddress "Recompensa de Mineria" "bech32")
-#
+echo
+echo "Punto 2.4. Crear direccion desde Miner con etiqueta Recompensa de Mineria"
+Mineria=$(bitcoin-cli -rpcwallet=Miner getnewaddress "Mineria")
+echo "Direccion mineria $Mineria"
+
 #Cuantos bloques se necesitan para obtener un saldo positivo
-alturaini=$(bitcoin-cli getblockchaininfo | jq -r '.blocks')
-bitcoin-cli generatetoaddress 105 $(Mineria) #Extraer nuevo bloques generatetoaddress "Recompensa de Mineria"
-until { bitcoin-cli getbalance != 0} ; do
-    sleep 5 #esperamos
+echo
+echo "Punto 2.5. Extraer bloques hasta que Miner tenga saldo positivo"
+balance=0
+until [ $balance -ge 1 ] ; do
+    bitcoin-cli generatetoaddress 1 "$Mineria"
+    p=$(bitcoin-cli -rpcwallet=Miner getbalance | bc)
+    balance=${p%.*}
 done
-alturafin=$(bitcoin-cli getblockchaininfo | jq -r '.blocks')
-altura= $(alturafin) - $(alturaini)
+altura=$(bitcoin-cli getblockchaininfo | jq -r '.blocks')
+echo
+echo "Punto 2.6. Cuanto ha tardado en tener saldo y por que"
 echo "Ha tardado $altura bloques en obtener saldo positivo"
 echo "La billetera obtiene un saldo positivo porque hay que esperar 100 bloques para poder gastar lo minado"
-echo "Puse una techa para continuar"
-read
-#
+
 #Saldo de la billetera Miner
-echo "La billetera Miner tiene un balance de $(bitcoin-cli getbalance) BTC"
-#
+echo
+echo "Punto 2.7 Saldo de billetera Miner"
+echo "La billetera Miner tiene un balance de $(bitcoin-cli -rpcwallet=Miner getbalance) BTC"
+
 #Crear una direccion "Recibido" desde la billetera Trader
-bitcoin-cli unloadwallet "Miner"
-bitcoin-cli loadwallet "Trader"
-Recibido=$(bitcoin-cli getnewaddress "Recibido" "bech32")
-bitcoin-cli unloadwallet "Trader"
-bitcoin-cli loadwallet "Miner"
-#
+echo
+echo "Punto 3.1. Crear direccion en Trader con la etiqueta Recibido"
+Recibido=$(bitcoin-cli -rpcwallet=Trader getnewaddress "Recibido")
+echo "Direccion de Trader $Recibido"
+
 #Enviar 20BTC desde Miner a Trader
-Monto=$(bitcoin-cli listunspent | jq -r '.[0] | .amount') #Suponiendo que en la primera transaccion minada hay una recompensa de mas de 20BTC
-Dedireccion=$(bitcoin-cli listunspent | jq -r '.[0] | .address')
+echo
+echo "Punto 3.2. Enviar 20BTC de Miner a Trader"
+Monto=$(bitcoin-cli -rpcwallet=Miner listunspent | jq -r '.[0] | .amount') #Suponiendo que en la primera transaccion minada hay una recompensa de mas de 20BTC
+Dedireccion=$(bitcoin-cli -rpcwallet=Miner listunspent | jq -r '.[0] | .address')
 MontoCambio=$(echo "($Monto - 20.001)" | bc) #Le quito los 20 y el pago del minero
-#
+
 #Supongamos que es un solo utxo de mas de 20BTC
-Cambio=$(bitcoin-cli getnewaddress "Cambio" "bech32")
-utxo0itxd=$(bitcoin-cli listunspent | jq -r '.[0] | .txid')
-utxo0vout=$(bitcoin-cli listunspent | jq -r '.[0] | .vout')
-rawtxhex=$(bitcoin-cli createrawtransaction '''[ { "txid": "'$utxo0itxd'", "vout": '$utxo0vout' } ]''' '''[{ "'$Recibido'": 20 },{ "'$Cambio'": '$MontoCambio' }]''') #La diferencia es del minero
-firmadotx=$(bitcoin-cli signrawtransactionwithwallet $rawtxhex | jq -r '.hex')
+Cambio=$(bitcoin-cli -rpcwallet=Miner getnewaddress "Cambio")
+utxo0itxd=$(bitcoin-cli -rpcwallet=Miner listunspent | jq -r '.[0] | .txid')
+utxo0vout=$(bitcoin-cli -rpcwallet=Miner listunspent | jq -r '.[0] | .vout')
+rawtxhex=$(bitcoin-cli -rpcwallet=Miner createrawtransaction '''[ { "txid": "'$utxo0itxd'", "vout": '$utxo0vout' } ]''' '''[{ "'$Recibido'": 20 },{ "'$Cambio'": '$MontoCambio' }]''') #La diferencia es del minero
+firmadotx=$(bitcoin-cli -rpcwallet=Miner signrawtransactionwithwallet $rawtxhex | jq -r '.hex')
 identtx=$(bitcoin-cli sendrawtransaction $firmadotx)
-#
+
 #Ver en la mempool
+echo
+echo "Punto 3.3. Obtener la transaccion no confirmada desde el mempool con getmempoolentry"
 echo "Se ha transferido 20 BTC de la billetera Miner a la billetera Trader. A continuacion se muestran los datos de la transaccion en la mempool"
 bitcoin-cli getmempoolentry $identtx
-echo "Puse una techa para continuar"
-read
-#
+
 #Minar un bloques
-hashbloque=$(bitcoin-cli generatetoaddress 1 $(Mineria))
-#
+echo
+echo "Punto 3.4. Confirmar la transaccion creando un bloque adicional"
+hashbloque=$(bitcoin-cli generatetoaddress 1 "$Mineria")
+
 #Datos de la transaccion
-fee=$(bitcoin-cli gettransaction $identtx | jq -r '.fee')
-Adireccion=$(bitcoin-cli gettransaction $identtx | jq -r '.details[0].address')
-Amonto=$(bitcoin-cli gettransaction $identtx | jq -r '.details[0].amount')
-Cambiodireccion=$(bitcoin-cli gettransaction $identtx | jq -r '.details[1].address')
-Cambiomonto=$(bitcoin-cli gettransaction $identtx | jq -r '.details[1].amount')
-Nconf=$(bitcoin-cli gettransaction $identtx | jq -r '.confirmations')
-Altura=$(bitcoin-cli gettransaction $identtx | jq -r '.blockheight')
+echo
+echo "Punto 3.5 Obtener los detalles de la transaccion"
 echo "La transaccion se ha minado, a continuacion se muestran los datos de la transaccion en la blockchain"
+echo "Usaremos el comando gettransaction y leeremos los datos con Json"
+fee=$(bitcoin-cli -rpcwallet=Miner gettransaction $identtx | jq -r '.fee')
+Adireccion=$(bitcoin-cli -rpcwallet=Miner gettransaction $identtx | jq -r '.details[0].address')
+Amonto=$(bitcoin-cli -rpcwallet=Miner gettransaction $identtx | jq -r '.details[0].amount')
+Cambiodireccion=$(bitcoin-cli -rpcwallet=Miner gettransaction $identtx | jq -r '.details[1].address')
+Cambiomonto=$(bitcoin-cli -rpcwallet=Miner gettransaction $identtx | jq -r '.details[1].amount')
+Nconf=$(bitcoin-cli -rpcwallet=Miner gettransaction $identtx | jq -r '.confirmations')
+Altura=$(bitcoin-cli -rpcwallet=Miner gettransaction $identtx | jq -r '.blockheight')
 echo "La identificacion de la transaccion es: $identtx"
 echo "Se envio desde la direccion de Miner: $Dedireccion"
 echo "La comision de la transaccion es: $fee"
@@ -118,20 +150,7 @@ echo -n "La direccion de cambio es $Cambiodireccion"
 echo "por un monto de $Cambiomonto"
 echo "El numero de confirmaciones es $Nconf"
 echo "El numero de bloque de la transaccion es $Altura"
-echo "La billetera Miner tiene un balance de $(bitcoin-cli getbalance) BTC"
-bitcoin-cli unloadwallet "Miner"
-bitcoin-cli loadwallet "Trader"
-echo "La billetera Trader tiene un balance de $(bitcoin-cli getbalance) BTC"
-echo -n "El programa ha finalizado, ¿Desea borrar los archivos de instalacion? (s/n)"
-read -N 1 continuar
-if [ $continuar = "s" ] ; then
-    rm /usr/local/bin/bitcoin-25.0-x86_64-linux-gnu.tar.gz
-    cd $directorio
-    rm SHA256SUMS.asc
-    rm SHA256SUMS
-else
-    cd $directorio #vuelvo al directorio donde empezó el script
-fi
-echo "Puse una techa para finalizar"
-read
+echo "La billetera Miner tiene un balance de $(bitcoin-cli -rpcwallet=Miner getbalance) BTC"
+echo "La billetera Trader tiene un balance de $(bitcoin-cli -rpcwallet=Trader getbalance) BTC"
+echo
 echo "Muchas gracias"
